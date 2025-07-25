80d4e2bcc40270443c3acb518c76573a84f41ca7
Yohai Wiener <yohai.wiener@eko.com>
2025-07-25 11:47:54 -0500

    fix: update pivot shot timeline

--- src/timelines.ts
+++ src/timelines.ts
@@ -34,24 +34,9 @@ export const TIMESLINES = {
   PIVOT: [
     {
       delay: 0,
-      duration: 10,
-      params: {
-        rotate: -45,
-        easeIn: {
-          duration: 2,
-          level: 1
-        },
-        easeOut: {
-          duration: 2,
-          level: 1
-        }
-      }
-    },
-    {
-      delay: 2,
       duration: 30,
       params: {
-        rotate: 90,
+        rotate: -90,
         easeIn: {
           duration: 2,
           level: 1
